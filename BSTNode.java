2c13dd7279bb1c7e1e1a637a57a47e577bbbc928
foreverTuesday <scheffm@simmons.edu>
2017-05-21 09:47:26 -0400

    Add files via upload

--- BSTNode.java
+++ BSTNode.java
@@ -3,32 +3,54 @@ public class BSTNode<E extends Comparable<E>> {
 
 	E element;
 	BSTNode<E> parent;
-	BSTNode<E> left;
-	BSTNode<E> right;
-
-	BSTNode(E e){ 
-		element = e;
-		left = null;
-		right = null;
-		parent = null;}
+	BSTNode<E> lc;
+	BSTNode<E> rc;
 	
-	public void setElement(E e){ element = e;}
-	public void setParent(BSTNode<E> p){ parent = p; }
-	public void setLeft(BSTNode<E> l){left = l;}
-	public void setRight(BSTNode<E> r){right = r;}
 	
-	public E getElement(){return element;}
-	public BSTNode<E> getParent(){return parent;}
-	public BSTNode<E> getLeft(){return left;}
-	public BSTNode<E> getRight(){return right;}
-	
-	public String toString(){
-      String s = "Element: "+element.toString();
-      if(parent!=null){ s += "\nParent: "+parent.getElement();}
-      if(left!=null) { s+="\nLeft: "+left.getElement();}
-      if(right!=null){s+="\nRight: "+right.getElement();}
-		return s;
+	BSTNode(E e){
+		element=e;
+		lc = null;
+		rc = null;
+		parent = null;
 	}
 	
-}
 
+//gets and sets	
+	public void setElement(E e){
+		element=e;
+	}
+	
+	public void setParent(BSTNode<E>p){
+		parent=p;
+	}
+	
+	public void setLeftChild(BSTNode<E>l){
+		lc = l;
+	}
+	
+	public void setRightChild(BSTNode<E>r){
+		rc = r;
+	}
+	
+	public E getElement(){
+		return element;
+	}
+	
+	public BSTNode<E> getParent(){
+		return parent;
+	}
+	
+	public BSTNode<E> getLeftChild(){
+		return lc;
+	}
+	
+	public BSTNode<E> getRightChild(){
+		return rc;
+	}
+	
+	public String toString(){
+		return element.toString();
+	}
+	
+	
+}
